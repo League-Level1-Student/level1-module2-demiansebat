b44cc78aac463ff11f662642d0b6ac2547dd5525
root <root@da41696d5125>
2020-05-10 18:27:46 +0000

    Minion ip

---
+++ src/_06_minion/Minion.java
@@ -0,0 +1,22 @@
+package _06_minion;
+
+public class Minion {
+private String name; 
+	private int eyes; 
+	private String color; 
+	private String master;
+	Minion(String name, int eyes, String color,String master){
+		this.name= name;
+		this.eyes= eyes;
+		this.color=color;
+		this.master=master;
+	}
+	String getname() {
+		return name;
+	}
+	String geteyes() {
+		return eyes;
+		
+	}
+	}
+	
